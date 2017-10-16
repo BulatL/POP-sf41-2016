0987e8c8bd2be531ff96b22a1d44e2a9cea85e07
student <student@ftn.ns>
2017-10-16 12:27:49 +0200

    init

---
+++ POP-sf41_2016/Program.cs
@@ -0,0 +1,22 @@
+﻿using ProstorImena;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POP_sf41_2016
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A a = new A();
+
+            a.SetNaziv("naziv svih naziva");
+
+            Console.WriteLine("Naziv je" + a.GetNaziv());
+            Console.ReadLine();
+        }
+    }
+}
